3372534f3dafded0d71e112e29a618f5559a7400
Oleksandr <rezikun@gmail.com>
2025-02-15 18:02:21 +0100

    refactor dataset and expand it

--- trainv11.py
+++ trainv11.py
@@ -2,18 +2,19 @@ from ultralytics import YOLO
 
 
 def train():
-    model = YOLO('yolo11n.pt')
+    model = YOLO('yolo11s.pt')
 
-    results = model.train(data='datasets/dataset.yaml', epochs=2)
+    results = model.train(
+      data='datasets/dataset.yaml', 
+      epochs=200, device=0, 
+      save_period=5, 
+      project="/content/gdrive/MyDrive/Runs")
     print(results)
 
     # Evaluate the model's performance on the validation set
     results = model.val()
     print(results)
 
-    # Perform object detection on an image using the model
-    results = model("short_close_range.mp4")
-    print(results)
     # Export the model to ONNX format
     success = model.export(format="onnx")
 
